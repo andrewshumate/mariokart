41dc2837c69e12a4481e8101ae3a6478149c7d42
andrewshumate <shumate@cs.washington.edu>
2023-07-16 17:22:38 -0700

    Add extra filters

--- scraper.js
+++ scraper.js
@@ -4,7 +4,13 @@
  ** To use this, copy-paste this entire file into the console on this page:
  ** https://www.mariowiki.com/Mario_Kart_8_Deluxe_in-game_statistics
  **
- ** Copy the log message (right click > "Copy Object") into a CSV file.
+ ** The script will output a log message in CSV format which contains every
+ ** Pareto optimal combo. Copy the log message (right click > "Copy Object")
+ ** into a CSV file.
+ **
+ ** The pair of stats to consider (e.g. speed and mini-turbo) can be changed
+ ** near the bottom of this file. Multiple Parteo optimality filters can be
+ ** stacked on top of each other (order matters).
  **/
 
 function extractTableData(table) {
@@ -104,30 +110,7 @@ function extractTablesData(tables) {
 
 var tables = document.querySelectorAll("table");
 var extractedTablesData = extractTablesData(tables);
-
 var { drivers, bodies, tires, gliders } = extractedTablesData;
-
-// Create the header row
-var header = [
-  "driver",
-  "kart",
-  "tires",
-  "glider",
-  "weight",
-  "acceleration",
-  "on road traction",
-  "off road traction",
-  "mini-turbo",
-  "ground speed",
-  "water speed",
-  "anti-gravity speed",
-  "air speed",
-  "ground handling",
-  "water handling",
-  "anti-gravity handling",
-  "air handling",
-  "invincibility",
-];
 
 // Generate all possible combinations of the four categories.
 // Each combo represents a row in the output CSV.
@@ -152,7 +135,49 @@ for (const driver of drivers) {
   }
 }
 
-function calculateParetoOptimality(stat1, stat2) {
+// Create the header row
+var Stat = {
+  driver: "driver",
+  kart: "kart",
+  tires: "tires",
+  glider: "glider",
+  weight: "weight",
+  acceleration: "acceleration",
+  onRoadTraction: "on road traction",
+  offRoadTraction: "off road traction",
+  miniTurbo: "mini-turbo",
+  groundSpeed: "ground speed",
+  waterSpeed: "water speed",
+  antiGravitySpeed: "anti-gravity speed",
+  airSpeed: "air speed",
+  groundHandling: "ground handling",
+  waterHandling: "water handling",
+  antiGravityHandling: "anti-gravity handling",
+  airHandling: "air handling",
+  invincibility: "invincibility",
+};
+var header = [
+  Stat.driver,
+  Stat.kart,
+  Stat.tires,
+  Stat.glider,
+  Stat.weight,
+  Stat.acceleration,
+  Stat.onRoadTraction,
+  Stat.offRoadTraction,
+  Stat.miniTurbo,
+  Stat.groundSpeed,
+  Stat.waterSpeed,
+  Stat.antiGravitySpeed,
+  Stat.airSpeed,
+  Stat.groundHandling,
+  Stat.waterHandling,
+  Stat.antiGravityHandling,
+  Stat.airHandling,
+  Stat.invincibility,
+];
+
+function removeParetoSuboptimalCombos(stat1, stat2) {
   header.push(`${stat1} + ${stat2}`);
   header.push("PO?");
   const stat1Index = header.indexOf(stat1);
@@ -224,9 +249,18 @@ function calculateParetoOptimality(stat1, stat2) {
       }
     }
   });
+
+  // Remove sub-optimal combos
+  combinations = combinations.filter(
+    (combo) => combo[paretoOptimalIndex] === true
+  );
 }
 
-calculateParetoOptimality("mini-turbo", "ground speed");
+// Choose the pair(s) of stats to consider here. Multiple Parteo optimality
+// filters can be stacked on top of each other (order matters).
+removeParetoSuboptimalCombos(Stat.groundSpeed, Stat.miniTurbo);
+removeParetoSuboptimalCombos(Stat.acceleration, Stat.invincibility);
+removeParetoSuboptimalCombos(Stat.groundHandling, Stat.weight);
 
 // Print the header and combinations as a single string with line breaks
 var output = [header, ...combinations.map((row) => row.join(","))].join("\n");
